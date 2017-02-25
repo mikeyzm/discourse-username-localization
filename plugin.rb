fffba0283c47d700a25a0e8cf7291bca6c4fbfea
Mike <48353088@qq.com>
2017-02-25 14:23:01 +0800

    Update

--- plugin.rb
+++ plugin.rb
@@ -49,6 +49,25 @@ after_initialize do
     end
   end
 
+  UserNameSuggester.class_eval do
+    def self.sanitize_username(name)
+      name = ActiveSupport::Inflector.transliterate(name)
+      # 1. replace characters that aren't allowed with '_'
+      name.gsub!(UsernameValidator::CONFUSING_EXTENSIONS, "_")
+      name.gsub!(/[^\w\u4E00-\u9FD5\u3400-\u4DBF\u{20000}-\u{2A6DF}\u{2A700}-\u{2CEAF}\uF900–\uFAFF\u{2F800}-\u{2FA1D}\uAC00–\uD7AF\u3040-\u30FF\u31F0–\u31FF\u{1B000}–\u{1B0FF}\u3005.-]/, "_")
+      # 2. removes unallowed trailing characters
+      name = remove_unallowed_trailing_characters(name)
+      # 3. unify special characters
+      name.gsub!(/[-_.]{2,}/, "_")
+      name
+    end
+
+    def self.remove_unallowed_trailing_characters(name)
+      name.gsub!(/[^\w\u4E00-\u9FD5\u3400-\u4DBF\u{20000}-\u{2A6DF}\u{2A700}-\u{2CEAF}\uF900–\uFAFF\u{2F800}-\u{2FA1D}\uAC00–\uD7AF\u3040-\u30FF\u31F0–\u31FF\u{1B000}–\u{1B0FF}\u3005]/, "")
+      name
+    end
+  end
+
   LetterAvatar::Identity.class_eval do
     def self.from_username(username)
       identity = new
